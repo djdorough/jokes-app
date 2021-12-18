41a09d2c750afb607fc688250fd294a0b83806d4
djdorough <96351896+djdorough@users.noreply.github.com>
2021-12-18 12:29:42 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@
 $host = "l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "ta05kk1hap1elym3";
 $user_pass = "xvcy55rv7zguvgst";
-$database_in_use = "Jokes";
+$database_in_use = "zbf8u5wldc3vg235";
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
 
